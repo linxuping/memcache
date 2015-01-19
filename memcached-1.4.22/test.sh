28b3ab491587701e95d326d890fac9c9541e1adc
linxuping <linxuping@126.com>
2015-01-19 22:30:25 +0800

    update set mykey

--- memcached-1.4.22/test.sh
+++ memcached-1.4.22/test.sh
@@ -1,13 +1,11 @@
 #./memcached -p 11211 -t 2 | grep lxp
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
-echo "get mykey" | nc localhost 11211 &
+printf "set mykey0 0 1000 6\r\nresul0\r\n" |nc localhost 11211
+printf "set mykey1 0 1000 6\r\nresul0\r\n" |nc localhost 11211
+printf "set mykey2 0 1000 6\r\nresul0\r\n" |nc localhost 11211
+printf "set mykey3 0 1000 6\r\nresul0\r\n" |nc localhost 11211
+printf "set mykey4 0 1000 6\r\nresul0\r\n" |nc localhost 11211
+printf "set mykey5 0 1000 6\r\nresul0\r\n" |nc localhost 11211
+printf "set mykey6 0 1000 6\r\nresul0\r\n" |nc localhost 11211
+printf "set mykey7 0 1000 6\r\nresul0\r\n" |nc localhost 11211
+printf "set mykey8 0 1000 6\r\nresul0\r\n" |nc localhost 11211
+printf "set mykey9 0 1000 6\r\nresul0\r\n" |nc localhost 11211
